7208003f660fce2bae3af20ab0cd240426496528
Madalena Marcelino <madelynbennet@gmail.com>
2018-11-10 14:36:40 -0500

    updated b, updated n, switch not solved

--- ICrollet.c
+++ ICrollet.c
@@ -6,15 +6,18 @@ int main() {
 	int bal = 100; // initial balance variable
 	int bet; // initial bet variable
 	int curbal; // ballence after the player has placed a bet
+	int defeat = 0; // variable so you can exit the game
+	int num;
+	int numcasino;
 	char opc; // variable for chosen option
-
+	char*s;
+	
 
 	srand((unsigned int) time(NULL));
 
 	printf("Welcome to out roullete game!\n"); //introduction
-	printf("Balance:%d €\n", bal); //information on your current balance
-	printf("Current bet:%d €\n", bet); //information on how much money you habe put on the line
-
+	printf("Balance:%d \n", bal); //information on your current balance
+	printf("Current bet:%d \n", bet); //information on how much money you habe put on the line
 	/*list of options of possible moves in this game*/
 	printf("\nChoose one of the following options:\n"); 
 	printf("b - Start betting\n");
@@ -30,37 +33,56 @@ int main() {
 	printf("\n Select an option:\n");
 	
 
-	while(1) {
-/**/	scanf("%c", &opc); //read the option inserted
+	while(defeat == 0) {
+	opc = getchar(); //read the option inserted
 
 		switch(opc) {
 
 			case 'b':
 				printf("How much money do you want to throw away? "); 
 				scanf("%d", &bet); //to save the amount of money the player wants to bet
-
-				if(bet == 0) { // if the player tries to play without using money
+				sscanf("%s",s);
+				if(bet <= 0) { // if the player tries to play without using money
 					printf("You can't play the game like this, use some money next time, use b to try again.\n");
 				} 
 				else if(bet > bal) { // if the player bets more than what he has
-					printf("Woah there buckaroo, you do not have that much on your account, try again with b\n");
+					printf("Woah there buckaroo, stop playing around and bet some actual money, try again with b\n");
 
 				} 
 				else if(bet <= bal) { //
-					printf("you have selected %d to use on this round", bet);
+					printf("you have selected %d to use on this round\n", bet);
 					curbal = bal - bet;
-					printf(", your current balance is %d\n To continue select another option", curbal);
+					printf(", your current balance is %d\n To continue select another option\n", curbal);
 
 				}
 
-				scanf("%c", &opc);
-
-				if(opc == 'n') {
-					
-				}
+				/*n = getchar();
+				switch(n) {
+					case 'n':    ///////wudduheck
+						function();
+						break;
+				}*/
 				
 				break; //help
 			case 'n':
+				int numcasino = rand()%36; //random number generator for all numbers between 0 and 35
+
+				printf("Alright, Feelin lucky i see, which number do you wish to bet? Choises between 1/35...\n");
+				scanf("%d", &num);
+
+
+				if(bet <= 0) { // in case the player starts witth n before b
+					printf("You can't play the game like this, use some money next time, use b to try again.\n"); //error message
+				} 
+				else if(num == numcasino) {
+					curbal = bet * 35;
+					printf("%d\n", numcasino);
+					printf("Congratulations, you won! lucky you, you got 35 times the money you bet.\n");
+					printf("to check your ballance press s\n");
+
+				}
+
+
 				break;
 			case 'e':
 				break;
